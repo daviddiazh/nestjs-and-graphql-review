1d81e6f19e37e57c5993f493ebe46c63f84bf448
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-06 17:24:12 -0500

    Adding settings of GraphQL and apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { HelloworldModule } from './hello-world/helloworld.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ]
+    }),
+    HelloworldModule
+  ],
   controllers: [],
   providers: [],
 })
